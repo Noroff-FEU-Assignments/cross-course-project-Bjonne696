45df6ff836f69420b27de72de055e066612d2399
Bjorn_Tore <Bjonne696@gmail.com>
2022-06-05 20:31:33 +0200

    update

--- js/creation.js
+++ js/creation.js
@@ -1,7 +1,9 @@
 const name = document.getElementById('name')
+const address = document.getElementById('address')
 const phone = document.getElementById('phone')
 const email = document.getElementById('email')
-const message = document.getElementById('message')
+const creditCard = document.getElementById('credit card')
+const bio = document.getElementById('bio')
 const form = document.getElementById('form')
 const errorElement = document.getElementById('error')
 
@@ -11,6 +13,12 @@ form.addEventListener('submit', (e) => {
     messages.push('Please enter your name, at least 4 characters.')
   }
 
+  let messages = []
+  if (name.value === '' || name.value == null) {
+    messages.push('Please a valid address, at least 4 characters.')
+  }
+  
+
   if (phone.value === '' || phone.value == null) {
     messages.push('phone number is required')
   }
@@ -19,7 +27,11 @@ form.addEventListener('submit', (e) => {
     messages.push('Email is required')
   }
 
-  if (messages.length > 0) {
+  if (email.value === '' || email.value == null) {
+    messages.push('Credit Card is required')
+  }
+
+  if (bio.length > 0) {
     e.preventDefault()
     errorElement.innerText = messages.join(', ')
   }
